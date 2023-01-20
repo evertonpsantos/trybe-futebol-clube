6e26a384ed1365eea10361105d59ed454fc5c946
evertonpsantos <evertonpsantos8@gmail.com>
2023-01-20 20:16:03 -0300

    feat: add User migration and seeders

---
+++ app/backend/src/database/migrations/01-create-users.js
@@ -0,0 +1,32 @@
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    return queryInterface.createTable('users', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      username: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      role: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      },
+    })
+  },
+
+  async down(queryInterface) {
+    return queryInterface.dropTable('users');
+  },
+};
