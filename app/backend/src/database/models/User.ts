bd46cf0bdfb88971b4902e33359f187746b06d37
evertonpsantos <evertonpsantos8@gmail.com>
2023-01-20 18:49:42 -0300

    feat: create User model

---
+++ app/backend/src/database/models/User.ts
@@ -0,0 +1,37 @@
+import { Model, DataTypes } from 'sequelize';
+import sequelize from '.';
+
+class User extends Model {
+  declare id: number;
+  declare username: string;
+  declare role: string;
+  declare email: string;
+  declare password: string;
+}
+
+User.init({
+  id: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  username: {
+    type: DataTypes.STRING,
+  },
+  role: {
+    type: DataTypes.STRING,
+  },
+  email: {
+    type: DataTypes.STRING,
+  },
+  password: {
+    type: DataTypes.STRING,
+  },
+}, {
+  sequelize,
+  tableName: 'users',
+  timestamps: false,
+});
+
+export default User;
